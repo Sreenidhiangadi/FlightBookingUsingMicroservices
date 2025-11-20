4cd545bbef6b947ba85ef24bcec435e87ef62705
Sreenidhiangadi <134719947+Sreenidhiangadi@users.noreply.github.com>
2025-11-20 23:57:31 +0530

    Delete FirstProject directory

--- FirstProject/src/Excpetions/InvalidNameRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package Excpetions;
-
-public class InvalidNameRuntimeException extends RuntimeException {
-    public InvalidNameRuntimeException(String message) {
-        super(message);
-    }
-}
