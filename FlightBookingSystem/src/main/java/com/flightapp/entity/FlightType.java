cfdc0d2e8943091c3557431ad94afb53d307f196
Sreenidhiangadi <134719947+Sreenidhiangadi@users.noreply.github.com>
2025-11-20 23:57:45 +0530

    Delete FlightBookingSystem directory

--- FlightBookingSystem/src/main/java/com/flightapp/entity/FlightType.java
+++
@@ -1,6 +0,0 @@
-package com.flightapp.entity;
-
-public enum FlightType {
-	ONE_WAY,
-    ROUND_TRIP
-}
--- FlightBookingSystem/src/main/java/com/flightapp/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flightapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.flightapp.entity.Passenger;
-
-public interface PassengerRepository extends JpaRepository<Passenger,Long> {
-
-}
