97a157ee24045c62f5266e094f3e00b169bf12a5
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-11-28 17:41:46 +0530

    added controllertest

--- Nov_27/Microservices_demo-2/src/test/java/com/demo/controller/QuizControllerTest.java
+++ Nov_27/Microservices_demo-2/src/test/java/com/demo/controller/QuizControllerTest.java
@@ -1,24 +1,28 @@
 package com.demo.controller;
 
-import com.demo.entity.QuestionWrapper;
-import com.demo.entity.QuizDto;
-import com.demo.entity.Response;
-import com.demo.service.QuizService;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.*;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.demo.entity.QuestionWrapper;
+import com.demo.entity.QuizDto;
+import com.demo.service.QuizService;
 
 @WebMvcTest(QuizController.class)
 class QuizControllerTest {
